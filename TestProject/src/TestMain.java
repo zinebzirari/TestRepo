e0e25c54dcbdbf1328a51e5dd9f145a7b8b3c550
Zineb Zirari <zinebzirari14@augustana.edu>
2017-08-28 13:54:32 -0500

    Started the new Yak application and printed bye

--- TestProject/src/TestMain.java
+++ TestProject/src/TestMain.java
@@ -3,6 +3,7 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Emu World");
+		System.out.println("Goodbye");
 	}
 
 }
---
+++ TestProject/src/Yak.java
@@ -0,0 +1,8 @@
+
+public class Yak {
+
+	public static void main(String[] args) {
+		System.out.println("Yaks are better than emus");
+	}
+
+}
